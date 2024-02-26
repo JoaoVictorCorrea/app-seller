4aac9274435df675ecb82765cec9d0b578bb1972
João Victor Correa <correa.joaov25@gmail.com>
2024-02-26 14:42:50 -0300

    Initial commit

---
+++ backend/src/main/java/com/project/appseller/AppSellerApplication.java
@@ -0,0 +1,13 @@
+package com.project.appseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSellerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/appseller/AppSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.appseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
