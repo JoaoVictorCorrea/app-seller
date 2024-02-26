3a8edc45882b69b21ad78f6c78934b859a5bc470
João Victor Correa <correa.joaov25@gmail.com>
2024-02-26 15:39:06 -0300

    closes #1 - Create class Seller and Configurated H2 Database

---
+++ backend/src/main/java/com/project/appseller/models/Seller.java
@@ -0,0 +1,85 @@
+package com.project.appseller.models;
+
+public class Seller {
+    
+    private Long id;
+    private String name;
+    private Double salary;
+    private Double bonus;
+    private Integer gender;
+
+    public Seller(){}
+
+    public Seller(Long id, String name, Double salary, Double bonus, Integer gender) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.bonus = bonus;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(Double bonus) {
+        this.bonus = bonus;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+    
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Seller other = (Seller) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
